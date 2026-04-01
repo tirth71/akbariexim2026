9d39b6233c0733c9883a674aeea4a656016bcc48
kishan20205 <kishansavaliya258@gmail.com>
2026-04-01 17:35:21 +0530

    Revert "links"
    This reverts commit a6d2775e80f1859c3409e47636d52d470be265a8.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -149,10 +149,10 @@ const Footer = () => (
               info@akbariexim.com
             </a>
 
-            <a href="tel:+919687391793" className="flex gap-3">
+            <li className="flex gap-3">
               <Phone className="w-4 h-4 text-primary" />
               +91 93164 90925
-            </a>
+            </li>
 
             <li className="flex gap-3">
               <MapPin className="w-4 h-4 text-primary" />
